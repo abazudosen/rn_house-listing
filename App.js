ff583bc1f019726a47038b204510166c20a295f3
abaz <abaz@abaz.local>
2020-09-13 12:22:09 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Home from './Screens/Home';
+import Detail from './Screens/Detail';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Settings from './Screens/Settings';
+import { Ionicons } from '@expo/vector-icons';
+import {Platform} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: Home,
+    Settings,
+    navigationOptions: {title: 'Home'}
   },
+  Detail: {
+    screen: Detail,
+    navigationOptions: {title: 'Detail'}
+  },
+})
+
+const SettingsStack = createStackNavigator({
+  Settings: {
+    screen: Settings,
+    navigationOptions: {title: 'Settings'}
+  }
 });
+
+const AppNavigator = createBottomTabNavigator({
+  Home: HomeStack,
+  Settings: SettingsStack,
+}, { 
+  initialRouteName: 'Home',
+  defaultNavigationOptions: ({navigation}) => ({
+    tabBarIcon: ({ tintColor }) => {
+      const {routeName } = navigation.state;
+
+      let iconName;
+      if(routeName === 'Home') {
+        iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-home`;
+      } else if (routeName === 'Settings') {
+        iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-settings`;
+      }
+      return <Ionicons name={iconName} size={20} color={tintColor} />
+    },
+    tabBarOptions: {
+      activeTintColor: 'blue',
+      inactiveTintColor: '#556'
+    }
+  })
+})
+
+export default createAppContainer(AppNavigator)
